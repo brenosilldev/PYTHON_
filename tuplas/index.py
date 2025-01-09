87b57e2da54f22657cbf94b05652692bf1be2b5d
breno-spartacode <brenosill@hotmail.com>
2025-01-09 19:18:09 -0300

    :build: docs Adicionado aulas sobre tuplas, juntamente com exercicios.

---
+++ tuplas/index.py
@@ -0,0 +1,51 @@
+"""
+    TUPLAS
+
+    São imutavel,não são alterados.
+"""
+
+# tupla  = (100,2000,3000,3600) 
+
+# print(tupla[0])
+
+# n1,n2,n3,n4 = tupla
+
+
+vendas = [
+    ('20/08/2020', 'iphone x', 'azul', '128gb', 350, 4000),
+    ('20/08/2020', 'iphone x', 'prata', '128gb', 1500, 4000),
+    ('20/08/2020', 'ipad', 'prata', '256gb', 127, 6000),
+    ('20/08/2020', 'ipad', 'prata', '128gb', 981, 5000),
+    ('21/08/2020', 'iphone x', 'azul', '128gb', 397, 4000),
+    ('21/08/2020', 'iphone x', 'prata', '128gb', 1017, 4000),
+    ('21/08/2020', 'ipad', 'prata', '256gb', 50, 6000),
+    ('21/08/2020', 'ipad', 'prata', '128gb', 4000, 5000),
+]
+
+faturamento = 0
+produtomaisvendido = ''
+quantidademaisvendida = 0
+cormasivendido = ''
+tamanhomaisvendido =''
+for venda in vendas:
+    data,produto,cor,tamanho,qtd,valor = venda
+    if 'iphone' in produto and data == '20/08/2020':
+        faturamento += qtd * valor 
+ 
+    if data == '21/08/2020':
+        if  qtd > quantidademaisvendida :
+            produtomaisvendido = produto
+            quantidademaisvendida = qtd
+            cormasivendido = cor
+            tamanhomaisvendido = tamanho
+
+
+print(f'O produto mais vendido foi {produtomaisvendido} com {quantidademaisvendida} da cor {cormasivendido} com o total de capacidade de {tamanhomaisvendido}')
+
+        
+
+
+
+    
+
+
